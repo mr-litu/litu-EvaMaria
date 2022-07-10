d972ba9831f4c407e64a0267e4c99bf8c753cf3a
mr-litu <109011199+mr-litu@users.noreply.github.com>
2022-07-10 09:55:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,12 +10,10 @@ class script(object):
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
-
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+    SOURCE_TXT ="""<b>ᴍᴀʀᴄᴜs ʙᴏᴛ</b>
+✰ ғᴏʀ ʀᴇᴘᴏ ᴏғ ʙᴏᴛ.\n ᴄᴏɴᴛᴀᴄᴛ @marcusxbot 
+<b>💞 sᴜᴘᴘᴏʀᴛ</b>
+- <a href=https://t.me/top10ner_0>ᴍᴏᴠɪᴇs ᴄʜᴀɴɴᴇʟ</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
